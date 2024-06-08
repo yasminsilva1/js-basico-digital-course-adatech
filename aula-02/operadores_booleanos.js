7a0f780659a3e6be02e7515b04fbaa00d521855b
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-06-08 17:52:39 -0300

    Adiciona exemplos e explicações de conjunções lógicas (and, or, not) em javascript

--- aula-02/operadores_booleanos.js
+++ aula-02/operadores_booleanos.js
@@ -28,3 +28,20 @@ console.log(numero1 >= "11"); // false
 
 // Menor ou igual (<=)
 console.log(9 <= numero2); // true
+
+// * Conjunções Lógicas
+const idadePessoa1 = 20;
+const idadePessoa2 = 39;
+
+// Conjunção Lógico AND (&&) - os dois lados da comparação devem ser verdadeiros para resultar em true!
+console.log(idadePessoa1 >= 18 && idadePessoa2 >= 18); // true
+console.log(idadePessoa1 < 18 && idadePessoa2 >= 18); // false
+
+// Conjunção Lógico OR (||) - um dos lados da comparação deve ser verdadeiro para resultar em true!
+console.log(idadePessoa1 < 18 || idadePessoa2 >= 18); // true
+
+// Operador de inversão NOT (!) - se for true, passa a ser false. Se for false, passa a ser true.
+console.log(!true); // false
+console.log(!false); // true
+
+console.log(!(idadePessoa1 >= 18)); // retorna true se a pessoa for menor de idade
