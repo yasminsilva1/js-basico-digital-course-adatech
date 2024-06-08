b038a6ab1f417616b357ddddcb08d771fef039b6
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-06-08 15:38:15 -0300

    Converter manualmente os dados (coerção explícita)

--- aula-01/entrada_de_dados_pelo_node.js
+++ aula-01/entrada_de_dados_pelo_node.js
@@ -2,4 +2,16 @@ const prompt = require("readline-sync");
 
 const idade = prompt.question("Qual eh sua idade?");
 
-console.log(idade);
+// * Sempre que o usuário retorna um dado através do prompt ou do input, o valor virá do tipo string.
+// Converter a idade de string para number
+const idadeNumber =  Number(idade);
+console.log(idadeNumber, typeof idadeNumber);
+
+// * COERÇÃO EXPLÍCITA (conversão manual)
+console.log(Number("x")) // NaN - not a number
+
+console.log(typeof String(10)); // string
+
+console.log(Boolean(2)); // true //* (qualquer número diferente de 0 é true!)
+
+console.log(Boolean(0)); // false
