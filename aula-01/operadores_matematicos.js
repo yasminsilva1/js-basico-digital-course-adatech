6b6e1ed0da5afab51efababe682d455d83be82d4
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-06-08 14:07:19 -0300

    Estudo sobre operações matemáticas e a biblioteca Math

---
+++ aula-01/operadores_matematicos.js
@@ -0,0 +1,16 @@
+const nota1 = 10;
+const nota2 = 8;
+
+const somaDasNotas = nota1 + nota2;
+
+let mediaDasNotas = somaDasNotas / 2;
+mediaDasNotas = (nota1 + nota2) / 2;
+
+console.log(`Soma das notas = ${somaDasNotas}`);
+console.log(`Média das notas = ${mediaDasNotas}`);
+
+const numero = 16;
+console.log(`Raiz quadrada de ${numero} = ${Math.sqrt(numero)}`);
+
+// Número aleatório
+console.log(Math.random());
