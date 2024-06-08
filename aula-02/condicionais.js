aa65848315d5b45abb3f6567f31aa8c97b15b733
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-06-08 18:51:04 -0300

    Adiciona um  exemplo da estrutura condicional switch case

--- aula-02/condicionais.js
+++ aula-02/condicionais.js
@@ -35,3 +35,20 @@ if (idadeDaPessoa >= 18 && temCNH) {
 } else {
 	console.log("Você não pode dirigir!");
 }
+
+// * Switch Case - muito útil quando sua variável possui valores específicos!
+const permissoes = "professor"; // "aluno" || "professor" || "admin"
+
+switch (permissoes) {
+	case "aluno":
+		console.log("Você só pode visualizar as aulas.");
+		break;
+	case "professor":
+		console.log("Você pode alterar as aulas e adicionar exercícios.");
+    break
+  case "admin":
+    console.log("Você pode fazer o que quiser no sistema.");
+    break
+  default:
+    console.log("Não sei quem você é no sistema.");
+}
